301106b622143b47b56005389c91fbc96066905f
userjack6880 <userjack6880@gmail.com>
2024-02-25 23:46:43 -0600

    update

--- main.cc
+++ main.cc
@@ -147,7 +147,7 @@ void server(int argc, char *argv[], int numProcessors) {
         int flag;
         int recvPacket;
 
-        cout << "waiting for data from clients" << endl;
+        // cout << "waiting for data from clients" << endl;
         MPI_Irecv(&recvPacket, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &request);
         MPI_Test(&request, &flag, &status);
 
@@ -157,7 +157,7 @@ void server(int argc, char *argv[], int numProcessors) {
           int solutionBuf[recvPacket];
           int source = status.MPI_SOURCE;
 
-          // cout << "recieved data from client " << source << endl;
+          cout << "recieved data from client " << source << endl;
           MPI_Recv(&indexBuf, recvPacket, MPI_INT, source, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           MPI_Recv(&solutionBuf, recvPacket, MPI_INT, source, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
