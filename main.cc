9e764a0ca03842039a817204d8e05bacc2be0ddd
John Bradley <jbradley@Pavehawk.HPC.MsState.Edu>
2024-02-28 13:38:26 -0600

    update

--- main.cc
+++ main.cc
@@ -104,15 +104,15 @@ void server(int argc, char *argv[], int numProcessors) {
   int decreasePacket = 0;
   int maxPacket = 1;
 
-  // if(argc > 3) {
-  //   packetSize = stoi(argv[3]);
-  // }
-  // if(argc > 4) {
-  //   increasePacket = stoi(argv[4]);
-  // }
-  // if(argc > 5) {
-  //   increasePacket = stoi(argv[5]);
-  // }
+  if(argc > 3) {
+    packetSize = stoi(argv[3]);
+  }
+  if(argc > 4) {
+    increasePacket = stoi(argv[4]);
+  }
+  if(argc > 5) {
+    increasePacket = stoi(argv[5]);
+  }
 
   // get the number of games from the input file
   input >> numGames;
