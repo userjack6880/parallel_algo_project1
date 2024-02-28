cf1a38a660267f63765deb88b55ed48140e11c6d
John Bradley <jbradley@Pavehawk.HPC.MsState.Edu>
2024-02-28 14:57:06 -0600

    update

--- run.sh
+++ run.sh
@@ -8,27 +8,27 @@ done
 # increase, no decrease
 for run in 01 02 04 08 16 32 64
 do
-  sbatch run$run.js 1 1 0 p1_no_inc_no_dec
+  sbatch run$run.js 1 1 0 p1_inc_no_dec
 done
 # increase, decrease
 for run in 01 02 04 08 16 32 64
 do
-  sbatch run$run.js 1 1 1 p1_no_inc_no_dec
+  sbatch run$run.js 1 1 1 p1_inc_dec
 done
 
 # starting packet 10
 # no increase
 for run in 01 02 04 08 16 32 64
 do
-  sbatch run$run.js 10 0 0 p1_no_inc_no_dec
+  sbatch run$run.js 10 0 0 p10_no_inc_no_dec
 done
 # increase, no decrease
 for run in 01 02 04 08 16 32 64
 do
-  sbatch run$run.js 10 1 0 p1_no_inc_no_dec
+  sbatch run$run.js 10 1 0 p10_inc_no_dec
 done
 # increase, decrease
 for run in 01 02 04 08 16 32 64
 do
-  sbatch run$run.js 10 1 1 p1_no_inc_no_dec
+  sbatch run$run.js 10 1 1 p10_inc_dec
 done
