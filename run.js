45a700191205787c7742b74abe07c5fa51630947
John Bradley <jbradley@Pavehawk.HPC.MsState.Edu>
2024-02-28 14:17:01 -0600

    update

--- run.js
+++ run.js
@@ -1,32 +1,8 @@
 #!/bin/bash
-
-# optional arguments
-numTasks=1
-minPacket=1
-incPacket=1
-decPacket=1
-resultName="results"
-
-if [ "$#" -ge 1 ]; then
-  numTasks="$1"
-fi
-if [ "$#" -ge 2 ]; then
-  resultName="$2"
-fi
-if [ "$#" -ge 3 ]; then
-  minPacket="$3"
-fi
-if [ "$#" -ge 4 ]; then
-  incPacket="$4"
-fi
-if [ "$#" -ge 5 ]; then
-  decPacket="$5"
-fi
-
 #SBATCH --account=class-cse4163
-#SBATCH --job-name=Work$(printf "%0*d" 2 $numTasks)P
+#SBATCH --job-name=Work$(printf "%0*d" 2 $1)P
 #SBATCH --nodes=1
-#SBATCH --ntasks=$numTasks
+#SBATCH --ntasks=$1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=00:20:00
 #SBATCH --partition=ptolemy
@@ -34,4 +10,4 @@ fi
 #SBATCH --qos=class-cse4163
 module load openmpi
 
-mpirun -np $numTasks ./project1 hard_sample.dat sol_hard.$(printf "%0*d" 2 $numTasks) $minPacket $incPacket $decPacket >& $resultName.$(printf "%0*d" 2 $numTasks)p
+mpirun -np $1 ./project1 hard_sample.dat sol_hard.$(printf "%0*d" 2 $1) $3 $4 $5 >& $2.$(printf "%0*d" 2 $1)p
