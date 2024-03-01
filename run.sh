f0f2e2f3beee74b91c34250c8277846252ce6061
John Bradley <jbradley@Pavehawk.HPC.MsState.Edu>
2024-03-01 09:27:50 -0600

    fixed issue with overwriting output

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ do
   # no increase
   for run in 01 02 04 08 16 32 64
   do
-    sbatch --output=/dev/null --job-name="run$run-p$start-no_inc-$2" run$run.js $start 0 p1_no_inc $1
+    sbatch --output=/dev/null --job-name="run$run-p$start-no_inc-$2" run$run.js $start 0 p${start}_no_inc $1
   done
   # increase
   for run in 01 02 04 08 16 32 64
   do
-    sbatch --output=/dev/null --job-name="run$run-p$start-inc-$2" run$run.js $start 1 p1_inc $1
+    sbatch --output=/dev/null --job-name="run$run-p$start-inc-$2" run$run.js $start 1 p${start}_inc $1
   done
 done
