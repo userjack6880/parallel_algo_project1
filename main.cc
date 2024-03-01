d6dcb56641c65afc9490bd29711f14faefc6f3d2
userjack6880 <userjack6880@gmail.com>
2024-02-29 20:27:15 -0600

    rewrite

--- main.cc
+++ main.cc
@@ -246,6 +246,7 @@ void server(int argc, char *argv[], int numProcessors) {
         // put record the solution states
         cout << "recording " << recvPacket << " solutions" << endl;
         for (int i = 0; i < recvPacket; i++) {
+          cout << "solution " << indexBuf[i] << ": " << solutionBuf[i] << endl;
           solutions[indexBuf[i]] = solutionBuf[i];
         }
       }
